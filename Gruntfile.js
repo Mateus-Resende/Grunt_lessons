e6b94a0c84db5ce75e06e8fc7d1dbe9af2e4d218
Mateus Resende <mateus.rsnd@gmail.com>
2016-02-27 02:37:09 -0300

    Finished task 3

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,50 @@
-module.exports = function(grunt) {
+
+module.exports = grunt => {
+
+  require('time-grunt')(grunt);
 
   grunt.initConfig({
+
+    clean: {
+      build: {
+        src: ['.tmp/*']
+      }
+    },
+
+    compass: {
+      dist: {
+        options: {
+          sassDir: 'styles',
+          cssDir: '.tmp/styles'
+        }
+      }
+    },
+
+    copy: {
+      main: {
+        expand: true,
+        src: 'scripts/*',
+        dest: '.tmp'
+      }
+    },
+
+    jscs: {
+      src: 'scripts/**/*.js',
+      options: {
+        config: '.jscsrc'
+      }
+    },
 
     jshint: {
       files: ['Gruntfile.js', 'scripts/**/*.js', 'test/**/*.js'],
       options: {
         jshintrc: '.jshintrc'
       }
+    },
+
+    watch: {
+      files: ['<%= jshint.files %>', 'styles/**/*.css'],
+      tasks: ['newer:jshint', 'wiredep', 'newer:sass', 'newer:jscs']
     },
 
     wiredep: {
@@ -20,19 +58,18 @@ module.exports = function(grunt) {
           bowerrc: '.bowerrc'
         }
       }
-    },
-
-    watch: {
-      files: ['<%= jshint.files %>', 'styles/**/*.css'],
-      tasks: ['jshint', 'wiredep']
     }
 
   });
 
-  grunt.loadNpmTasks('grunt-wiredep');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-compass');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-jscs');
+  grunt.loadNpmTasks('grunt-newer');
+  grunt.loadNpmTasks('grunt-wiredep');
 
-  grunt.registerTask('default', ['jshint', 'wiredep']);
-
+  grunt.registerTask('default', ['clean', 'compass', 'copy', 'jscs', 'jshint', 'wiredep']);
 };
