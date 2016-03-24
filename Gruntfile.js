c83a8fbcecbc48e2c5bc4eb51464cd468d7471f6
Mateus Resende <mateus.rsnd@gmail.com>
2016-03-24 11:24:21 -0300

    Included inner folders on compass

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,75 +1,74 @@
-
 module.exports = grunt => {
 
-  require('time-grunt')(grunt);
+    require('time-grunt')(grunt);
 
-  grunt.initConfig({
+    grunt.initConfig({
 
-    clean: {
-      build: {
-        src: ['.tmp/*']
-      }
-    },
+        clean: {
+            build: {
+                src: ['.tmp/*']
+            }
+        },
 
-    compass: {
-      dist: {
-        options: {
-          sassDir: 'styles',
-          cssDir: '.tmp/styles'
+        compass: {
+            dist: {
+                options: {
+                    sassDir: 'styles/**/*.scss',
+                    cssDir: '.tpm/styles/',
+                }
+            }
+        },
+
+        copy: {
+            main: {
+                expand: true,
+                src: 'scripts/*',
+                dest: '.tmp'
+            }
+        },
+
+        jscs: {
+            src: 'scripts/**/*.js',
+            options: {
+                config: '.jscsrc'
+            }
+        },
+
+        jshint: {
+            files: ['Gruntfile.js', 'scripts/**/*.js'],
+            options: {
+                jshintrc: '.jshintrc'
+            }
+        },
+
+        watch: {
+            files: ['<%= jshint.files %>', 'styles/**/*.css'],
+            tasks: ['newer:jshint', 'wiredep', 'newer:sass', 'newer:jscs']
+        },
+
+        wiredep: {
+            task: {
+                src: [
+                    'scripts/**/*.js',
+                    'styles/**/*.css'
+                ],
+                exclude: ['bootstrap.js'],
+                options: {
+                    bowerrc: '.bowerrc'
+                }
+            }
         }
-      }
-    },
 
-    copy: {
-      main: {
-        expand: true,
-        src: 'scripts/*',
-        dest: '.tmp'
-      }
-    },
+    });
 
-    jscs: {
-      src: 'scripts/**/*.js',
-      options: {
-        config: '.jscsrc'
-      }
-    },
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-jscs');
+    grunt.loadNpmTasks('grunt-newer');
+    grunt.loadNpmTasks('grunt-wiredep');
 
-    jshint: {
-      files: ['Gruntfile.js', 'scripts/**/*.js', 'test/**/*.js'],
-      options: {
-        jshintrc: '.jshintrc'
-      }
-    },
-
-    watch: {
-      files: ['<%= jshint.files %>', 'styles/**/*.css'],
-      tasks: ['newer:jshint', 'wiredep', 'newer:sass', 'newer:jscs']
-    },
-
-    wiredep: {
-      task: {
-        src: [
-          'scripts/**/*.js',
-          'styles/**/*.css'
-        ],
-        exclude: ['bootstrap.js'],
-        options: {
-          bowerrc: '.bowerrc'
-        }
-      }
-    }
-
-  });
-
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-compass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-jscs');
-  grunt.loadNpmTasks('grunt-newer');
-  grunt.loadNpmTasks('grunt-wiredep');
-
-  grunt.registerTask('default', ['clean', 'compass', 'copy', 'jscs', 'jshint', 'wiredep']);
+    grunt.registerTask('default', ['clean', 'compass', 'copy', 'jscs', 'jshint', 'wiredep']);
 };
